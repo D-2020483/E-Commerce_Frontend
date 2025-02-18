887538a6376ae2a3c92988f9cc12ac8304a4c25d
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-02-18 14:06:40 +0530

    Update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
             </div>
 
             {/* Right Section (Cart and User Info) */}
-            <div className="flex items-center space-x-4 ml-auto">
+            <div className="flex items-center space-x-4">
                 <span className="text-gray-600">{itemCount}</span>
                 <a href="#" className="text-gray-600 hover:text-black">Cart</a>
                 
