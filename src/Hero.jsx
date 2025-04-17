ad7a07b30c09cc815872a041e851b6d7a550a055
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-04-18 02:04:45 +0530

    changes

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,17 +1,22 @@
-
 function Hero() {
   return (
-    <section className="p-8 py-8
-    ">
-      <div className="grid grid-cols-2 rounded-md min-h-full bg-blue-50">
+    <section className="p-8 py-8">
+      <div className="grid grid-cols-2 rounded-md min-h-full bg-card">
         <div className="flex flex-col justify-center p-16 gap-4">
-          <span className="inline-block rounded-full px-2 text-sm w-fit bg-yellow-500">WEEKLY DISCOUNT</span>
-          <h1 className="text-6xl font-semibold">Premium Product Online Shop</h1>
-          <p>
+          <span className="inline-block rounded-full px-2 text-sm w-fit bg-accent text-accent-foreground">
+            WEEKLY DISCOUNT
+          </span>
+          <h1 className="text-6xl font-semibold text-foreground">
+            Premium Product Online Shop
+          </h1>
+          <p className="text-muted-foreground">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos
             suscipit est autem quia? Voluptatem?
           </p>
-          <a to="/shop" className="text-white bg-black rounded-md w-fit block font-medium py-2 px-4">
+          <a
+            to="/shop"
+            className="text-primary-foreground bg-primary rounded-md w-fit block font-medium py-2 px-4"
+          >
             Shop Now
           </a>
         </div>
