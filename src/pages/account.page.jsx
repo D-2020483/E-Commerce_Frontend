94841ebebe6e9d7d3098355bc97126bba0ab5ed6
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-04-19 08:48:31 +0530

    updates

--- src/pages/account.page.jsx
+++ src/pages/account.page.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { useNavigate } from "react-router"
 import {
   User2Icon,
   MailIcon,
